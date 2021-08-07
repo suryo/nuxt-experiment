9569c753ecffd6436d80fd2c6ceedfb5c95aa7dd
suryo <suryoatm@gmail.com>
2021-08-07 14:21:06 +0700

    add badge page

---
+++ pages/badge.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+  <h2>Example heading <b-badge>New</b-badge></h2>
+  <h3>Example heading <b-badge>New</b-badge></h3>
+  <h4>Example heading <b-badge>New</b-badge></h4>
+  <h5>Example heading <b-badge>New</b-badge></h5>
+  <h6>Example heading <b-badge>New</b-badge></h6>
+
+  <div class="text-center">
+  <b-button variant="primary">
+    Notifications <b-badge variant="light">4</b-badge>
+  </b-button>
+</div>
+
+<div class="text-center">
+  <b-button variant="primary">
+    Profile
+    <b-badge variant="light">9 <span class="sr-only">unread messages</span></b-badge>
+  </b-button>
+</div>
+
+<div>
+  <b-badge variant="primary">Primary</b-badge>
+  <b-badge variant="secondary">Secondary</b-badge>
+  <b-badge variant="success">Success</b-badge>
+  <b-badge variant="danger">Danger</b-badge>
+  <b-badge variant="warning">Warning</b-badge>
+  <b-badge variant="info">Info</b-badge>
+  <b-badge variant="light">Light</b-badge>
+  <b-badge variant="dark">Dark</b-badge>
+</div>
+</div>
+
+
+</template>
+
+<script>
+export default {}
+</script>
