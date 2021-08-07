a3a5e2ec8b828f89e370f0db49db366c964e74f5
suryo <suryoatm@gmail.com>
2021-08-07 14:25:51 +0700

    add button page

---
+++ pages/button.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+  <b-button>Button</b-button>
+  <b-button variant="danger">Button</b-button>
+  <b-button variant="success">Button</b-button>
+  <b-button variant="outline-primary">Button</b-button>
+
+  <div>
+  <b-button>I am a Button</b-button>
+  <b-button href="#">I am a Link</b-button>
+</div>
+
+<b-row>
+  <b-col lg="4" class="pb-2"><b-button size="sm">Small Button</b-button></b-col>
+  <b-col lg="4" class="pb-2"><b-button>Default Button</b-button></b-col>
+  <b-col lg="4" class="pb-2"><b-button size="lg">Large Button</b-button></b-col>
+</b-row>
+
+<div>
+  <b-button variant="primary">Primary</b-button>
+  <b-button variant="secondary">Secondary</b-button>
+  <b-button variant="success">Success</b-button>
+  <b-button variant="danger">Danger</b-button>
+  <b-button variant="warning">Warning</b-button>
+  <b-button variant="info">Info</b-button>
+  <b-button variant="light">Light</b-button>
+  <b-button variant="dark">Dark</b-button>
+</div>
+
+<div>
+  <b-button variant="outline-primary">Primary</b-button>
+  <b-button variant="outline-secondary">Secondary</b-button>
+  <b-button variant="outline-success">Success</b-button>
+  <b-button variant="outline-danger">Danger</b-button>
+  <b-button variant="outline-warning">Warning</b-button>
+  <b-button variant="outline-info">Info</b-button>
+  <b-button variant="outline-light">Light</b-button>
+  <b-button variant="outline-dark">Dark</b-button>
+</div>
+
+<div>
+  <b-button variant="link">Link</b-button>
+</div>
+
+</div>
+
+
+</template>
+
+<script>
+export default {}
+</script>
